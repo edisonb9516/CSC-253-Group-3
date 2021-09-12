55e0549650c8162a696c3bd60b11c667fbb74614
Adrian Lamar <89109263+KanedaTech@users.noreply.github.com>
2021-09-12 19:58:09 -0400

    Update Treasure.cs

--- Roguelike/Engine/Core/Items,Inventory/Treasure.cs
+++ Roguelike/Engine/Core/Items,Inventory/Treasure.cs
@@ -12,5 +12,15 @@ namespace Engine.Core.Items_Inventory
         public static List<Treasure> treasures = new List<Treasure>();
 
         // TODO add treasure
+        public int Gold { get; set; };
+        public int Silver { get; set; };
+        public string SilverBroach { get; set; };
+        public string MusicBox { get; set; };
+        public string CrystalOrb { get; set; };
+        public string Diamond { get; set; };
+        public int StaminaRing { get; set; };
+        public int ManaRing { get; set; };
+        public int DamageRing { get; set; };
+        public int EvadeRing { get; set; };
     }
 }
