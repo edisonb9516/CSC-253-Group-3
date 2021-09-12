10ec62dcb4b1d098711d2f8d3d6f93dafe723a4d
Adrian Lamar <89109263+KanedaTech@users.noreply.github.com>
2021-09-12 19:57:30 -0400

    Update Potion.cs

--- Roguelike/Engine/Core/Items,Inventory/Potion.cs
+++ Roguelike/Engine/Core/Items,Inventory/Potion.cs
@@ -11,5 +11,11 @@ namespace Engine.Core.Items_Inventory
         // List
         public static List<Potion> potions = new List<Potion>();
         // TODO potions
+        public int HealthPotion { get; set; };
+        public int ManaPotion { get; set; };
+        public int IncDamagePotion { get; set; };
+        public int IncStaminaPotion { get; set; };
+        public int IncEvadePotion { get; set; };
+        public int IncLuckPotion { get; set; };
     }
 }
