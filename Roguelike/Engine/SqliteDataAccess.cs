4a6eb84df4712bf9089984b7d2880ef012fc957c
Kevin Griffin <kevin.baseball97@yahoo.com>
2021-10-05 19:10:53 -0400

    Added class for accessing the sqlite database

---
+++ Roguelike/Engine/SqliteDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class SqliteDataAccess
+    {
+        public static List<Core.Player.Player> LoadPlayers()
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Core.Player.Player>("select * from Players", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        public static void SavePlayer(Core.Player.Player player)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                // search the player table for similar info before saving and overwrite if necessary
+                // todo sql statement in cnn.execute string pass to input the player save
+                cnn.Execute("", player);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+    }
+}
